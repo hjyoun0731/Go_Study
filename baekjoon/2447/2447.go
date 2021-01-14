0170785715e7c684223963d6f4be529b336b01f2
hjyoun <hjyoun0731@gmail.com>
2021-01-14 17:46:57 +0900

    print star -10 #2447

---
+++ baekjoon/2447/2447.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+)
+
+func makeStar(n int) [][]string {
+	stars := make([][]string, n)
+	for i := 0; i < n; i++ {
+		stars[i] = make([]string, n)
+	}
+	for x := 0; x < n; x++ {
+		for y := 0; y < n; y++ {
+			if x%3 == 1 && y%3 == 1 {
+				stars[x][y] = " "
+			} else {
+				stars[x][y] = "*"
+			}
+		}
+	}
+	return stars
+}
+
+func makeBlank(n int, blankSize int, p [][]string) [][]string {
+	if blankSize == 1 {
+		return p
+	}
+
+	for i := blankSize; i < n; i += blankSize * 3 {
+		for j := blankSize; j < n; j += blankSize * 3 {
+			for x := i; x < i+blankSize; x++ {
+				for y := j; y < j+blankSize; y++ {
+					p[x][y] = " "
+				}
+			}
+		}
+	}
+
+	return makeBlank(n, blankSize/3, p)
+}
+
+func printStar(n int) {
+	var strStar string
+	pattern := makeStar(n)
+	pattern = makeBlank(n, n/3, pattern)
+
+	for i := 0; i < n; i++ {
+		for _, i := range pattern[i] {
+			strStar += string(i)
+		}
+		strStar += "\n"
+	}
+	fmt.Print(strStar)
+}
+
+func main() {
+	var N int
+	_, _ = fmt.Scan(&N)
+	printStar(N)
+}
